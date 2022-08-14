f1db05acba52084320b547e5e19ad4e87f8e92bd
Mixdor <miguel-avila112-@outlook.com>
2022-08-14 18:44:41 -0500

    Decorate fragment, colors and styles

--- app/src/main/java/com/mixdor/covertpots/fragment/FragDetallesPlanta.kt
+++ app/src/main/java/com/mixdor/covertpots/fragment/FragDetallesPlanta.kt
@@ -1,17 +1,16 @@
 package com.mixdor.covertpots.fragment
 
 import android.app.Dialog
+import android.graphics.Color
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
 import android.widget.TextView
-import android.widget.Toolbar
+import androidx.core.content.ContextCompat
 import androidx.fragment.app.DialogFragment
 import com.ekn.gruzer.gaugelibrary.ArcGauge
-import com.ekn.gruzer.gaugelibrary.HalfGauge
 import com.mixdor.covertpots.R
 import com.mixdor.covertpots.model.mPlanta
 
@@ -29,21 +28,32 @@ class FragDetallesPlanta(plant: mPlanta) : DialogFragment() {
         val view = inflater.inflate(R.layout.frag_detalles_planta, container, false)
 
         val toolbar = view.findViewById<androidx.appcompat.widget.Toolbar>(R.id.fracToolbar)
+        val textSerial = view.findViewById<TextView>(R.id.fragTextVSerial)
         val gause1 = view.findViewById<ArcGauge>(R.id.fraghalfGauge)
         val gause2 = view.findViewById<ArcGauge>(R.id.fraghalfGauge2)
         val gause3 = view.findViewById<ArcGauge>(R.id.fraghalfGauge3)
         val gause4 = view.findViewById<ArcGauge>(R.id.fraghalfGauge4)
 
-        toolbar.title = planta.nombre + " #"+planta.id.toString()
+        val serialId = "#"+planta.id.toString()+"     "
+        textSerial.text = serialId
+
+        toolbar.title = planta.nombre
         toolbar.setNavigationOnClickListener {
             dismiss()
         }
+        toolbar.setTitleTextAppearance(requireContext(), R.style.fullDialogTitle)
+        toolbar.setTitleTextColor(Color.WHITE)
 
 
         gause1.value = planta.sHumS.toDouble()
         gause2.value = planta.sHumA.toDouble()
         gause3.value = planta.sIlu.toDouble()
         gause4.value = planta.sTemp.toDouble()
+
+        gause1.valueColor = ContextCompat.getColor(view.context, R.color.colorTexto)
+        gause2.valueColor = ContextCompat.getColor(view.context, R.color.colorTexto)
+        gause3.valueColor = ContextCompat.getColor(view.context, R.color.colorTexto)
+        gause4.valueColor = ContextCompat.getColor(view.context, R.color.colorTexto)
 
         return view
     }
